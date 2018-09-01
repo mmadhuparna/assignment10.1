fbf7e3ee744202e2eccae0671e176cf79db459fa
mmadhuparna <mmadhuparna@yahoo.com>
2018-09-01 19:31:32 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,15 @@
+//creating class
+class Rectangle {
+  constructor(length, breadth) {
+    this.length = length;
+    this.breadth = breadth;
+  }
+  //defining method
+  calculateArea(){
+    return this.length*this.breadth;
+    }
+  }  
+  //creating object         
+var x = new Rectangle(30, 20);
+//callin method in console
+console.log("Area of Rectangle:"+ " " + x.calculateArea());

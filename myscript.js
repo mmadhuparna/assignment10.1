ca15610bd2610e970511191952b72182ab8e8858
mmadhuparna <mmadhuparna@yahoo.com>
2018-09-02 17:08:53 +0530

    commit

--- myscript.js
+++ myscript.js
@@ -1,14 +1,14 @@
 //creating class
 class Rectangle {
-  constructor(length, breadth) {
+  constructor(length, breadth){
     this.length = length;
     this.breadth = breadth;
   }
   //defining method
   calculateArea(){
     return this.length*this.breadth;
-    }
-  }  
+  }
+}  
   //creating object         
 var x = new Rectangle(30, 20);
 //callin method in console
